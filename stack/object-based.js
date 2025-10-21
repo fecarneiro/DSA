a307fa5b27516641e1e87dfac88ab9e35a0b76a1
Felipe <fe.iacarneiro@gmail.com>
2025-10-21 14:09:21 -0300

    Add Node and Stack classes with basic methods; remove unused files

--- stack/object-based.js
+++ stack/object-based.js
@@ -1,28 +1,9 @@
-class Stack {
-  constructor() {
-    this.count = 0;
-    this.items = {};
-  }
-  push(element) {
-    this.items[this.count] = element;
-    this.count++;
-  }
-  pop(element) {
-    this.items.pop(element);
-  }
-  peak() {
-    return this.items[this.items.length - 1];
-  }
-  isEmpty() {
-    return this.items.length === 0;
-  }
-  size() {
-    return this.items.length;
-  }
-  clear() {
-    this.items = [];
-  }
-}
+import { Stack } from './stack.js';
+
 const stack = new Stack();
 stack.push('fe');
-stack;
+stack.push('feEE');
+stack.push('feEE');
+console.log(stack.peak());
+console.log(stack.isEmpty());
+console.log(stack.toString());
---
+++ stack/stack.js
@@ -0,0 +1,50 @@
+class Stack {
+  constructor() {
+    this.items = {};
+    this.count = 0;
+  }
+  push(element) {
+    this.items[this.count] = element;
+    this.count++;
+  }
+  pop() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    this.count--;
+    const result = this.items[this.count];
+    delete this.items[this.count];
+    return result;
+  }
+  peak() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[this.count - 1];
+  }
+  isEmpty() {
+    return this.count === 0;
+  }
+  size() {
+    return this.count;
+  }
+  clear() {
+    // this.items = {};
+    // this.count = 0;
+    while (!this.isEmpty()) {
+      this.pop();
+    }
+  }
+  toString() {
+    if (this.isEmpty()) {
+      return '';
+    }
+    let objString = `${this.items[0]}`;
+    for (let i = 1; i < this.count; i++) {
+      objString = `${objString}, ${this.items[i]}`;
+    }
+    return objString;
+  }
+}
+
+export { Stack };

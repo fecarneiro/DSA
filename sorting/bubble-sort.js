23a7d330dc07623a0105aca3a4f1e9968a16376b
Felipe <fe.iacarneiro@gmail.com>
2025-10-29 06:13:19 -0300

    add new feature

--- sorting/bubble-sort.js
+++ sorting/bubble-sort.js
@@ -1,21 +1,21 @@
 let arr = [7, 3, 9, 2];
+let n = arr.length;
+let swapped;
 
-//loop externo roda enquanto counter >0
-//counter reseta em cada passada -> loop verifica
-
-while (counter !== 0) {
-  let counter = 0;
-  for (let i = 0; i < arr.length - 1; i++) {
+do {
+  swapped = false;
+  for (let i = 0; i < n - 1; i++) {
     let temp = arr[i];
+
     if (arr[i] > arr[i + 1]) {
       arr[i] = arr[i + 1];
       arr[i + 1] = temp;
-      counter++;
+      swapped = true;
+      console.log(arr);
     }
   }
-}
-console.log(arr);
-console.log(counter);
+} while (swapped);
+
 // [ 3, 7, 2, 9 ] e counter = 2
 
 // Pega seu código do exercício 4.2/4.3 e coloca ele
---
+++ sorting/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+console.log(`round${i} `, arr);

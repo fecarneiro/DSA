0d0ef64afa6de649359dd2f3baa5949387e8302b
Felipe <fe.iacarneiro@gmail.com>
2025-11-01 10:56:36 -0300

    start binary search impl

--- search/binary-search.js
+++ search/binary-search.js
@@ -1,1 +1,18 @@
-const arr = [3, 4, 5, 6, 7, 8, 9];
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let startIndex = 0;
+let endIndex = arr.length - 1;
+let middleIndex = (endIndex - startIndex) / 2;
+const targetValue = 7;
+
+function binarySearch(startIndex, endIndex, middleIndex, targetValue) {
+  while (targetValue !== arr[middleIndex]) {
+    if (targetValue > arr[middleIndex]) {
+      startIndex = middleIndex;
+      middleIndex = startIndex + (endIndex - startIndex) / 2;
+    }
+  }
+  console.log(`matched value found at array index ${middleIndex}`);
+  return;
+}
+
+binarySearch(startIndex, endIndex, middleIndex, targetValue);
--- search/linear-search.js
+++ search/linear-search.js
@@ -1,22 +1,22 @@
+const arr = [63, 4, 25, 36, 7, 68, 79];
+const target = 7;
+const size = arr.length;
+const counter = 1;
+
 function linearSearch(arr, target, size, counter) {
   for (let i = 0; i < size; i++) {
     if (arr[i] == target) {
       console.log(
         `found at array index ${i}, after iterating ${counter} times`
       );
-      return i;
+      return;
     }
     counter++;
   }
   console.log(
     `array not found after iterating ${counter - 1} times (all array length)`
   );
-  return false;
+  return;
 }
-
-const arr = [63, 4, 25, 36, 7, 68, 79];
-const target = 7;
-const size = arr.length;
-const counter = 1;
 
 linearSearch(arr, target, size, counter);

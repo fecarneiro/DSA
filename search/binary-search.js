8c1a01841ca1b1169f1a2e55c48ace3dae981c5f
Felipe <fe.iacarneiro@gmail.com>
2025-11-01 11:59:19 -0300

    add new feature

--- search/binary-search.js
+++ search/binary-search.js
@@ -1,11 +1,14 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const targetValue = 9;
 let startIndex = 0;
 let endIndex = arr.length - 1;
 let middleIndex = (endIndex - startIndex) / 2;
-const targetValue = 7;
 
-function binarySearch(startIndex, endIndex, middleIndex, targetValue) {
-  while (targetValue !== arr[middleIndex]) {
+function binarySearch(targetValue, startIndex, endIndex, middleIndex) {
+  while (startIndex <= endIndex) {
+    if (targetValue == arr[middleIndex]) {
+      console.log(`target value at array index ${middleIndex}`);
+    }
     if (targetValue > arr[middleIndex]) {
       startIndex = middleIndex;
       middleIndex = startIndex + (endIndex - startIndex) / 2;
@@ -16,3 +19,18 @@ function binarySearch(startIndex, endIndex, middleIndex, targetValue) {
 }
 
 binarySearch(startIndex, endIndex, middleIndex, targetValue);
+// function binarySearch(targetValue, startIndex, endIndex, middleIndex) {
+//   while (targetValue !== arr[middleIndex]) {
+//     if (targetValue > arr[middleIndex]) {
+//       startIndex = middleIndex;
+//       middleIndex = startIndex + (endIndex - startIndex) / 2;
+//       if (endIndex - middleIndex == 1) {
+//         targetValue = endIndex;
+//       }
+//     }
+//   }
+//   console.log(`matched value found at array index ${middleIndex}`);
+//   return;
+// }
+
+// binarySearch(startIndex, endIndex, middleIndex, targetValue);

f30aa2aa84e496004c83ffc959bc4ea81eb9cd01
Felipe <fe.iacarneiro@gmail.com>
2025-11-01 10:09:11 -0300

    add new feature

--- search/linear-search.js
+++ search/linear-search.js
@@ -2,7 +2,7 @@ function linearSearch(arr, target, size, counter) {
   for (let i = 0; i < size; i++) {
     if (arr[i] == target) {
       console.log(
-        `found at array index: ${i} after iterating ${counter} times`
+        `found at array index ${i}, after iterating ${counter} times`
       );
       return i;
     }
@@ -14,8 +14,8 @@ function linearSearch(arr, target, size, counter) {
   return false;
 }
 
-const arr = [3, 4, 5, 6, 7, 8, 9];
-const target = 2;
+const arr = [63, 4, 25, 36, 7, 68, 79];
+const target = 7;
 const size = arr.length;
 const counter = 1;
 

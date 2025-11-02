3de1722cbc4c133138f7a25cae73a1abbb509970
Felipe <fe.iacarneiro@gmail.com>
2025-11-02 11:50:51 -0300

    add new feature

---
+++ sort/insertion-sort.js
@@ -0,0 +1,14 @@
+/*
+ * O(n²)
+ * Conceito de organizar cartas na mão
+ * Dividir mentalmente array em: parte organizada(esquerda) e desorganizada(direita)
+ * Um elemento por vez -> inserir na posição correta
+ * Consideramos arr[0] como já "organizado"
+ */
+
+function insertionSort(arr) {
+  for(i = 1; i < arr.length ; i++)
+
+}
+
+const arr = [3, 2, 5, 4, 1];
